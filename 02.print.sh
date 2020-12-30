28cdd26faba31caa89af92842847992ed79f888f
wazeb <mawazeb123456@gmail.com>
2020-12-30 18:23:48 +0530

    Trying print commant

--- 02.print.sh
+++ 02.print.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # We can print a message on screen using two commands in linux
-# 1. echo
-# 2. print
+# 1. echo   -> Preffered one because of less syntax.
+# 2. printf  -> Have more syntax
+
+echo Hello world
 
