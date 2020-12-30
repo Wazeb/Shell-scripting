57f1ca7b7b1332605e1ede8b3d9d0ac2a0a9e922
wazeb <mawazeb123456@gmail.com>
2020-12-30 23:48:52 +0530

    Veriables

--- 03-variables.sh
+++ 03-variables.sh
@@ -6,6 +6,6 @@ echo DevOps Tainer - Wazeb
 
 COURSE_NAME=DevOps
 
-echo Welcome to COURSE_NAME training
-echo COURSE_NAME Training Timings - 10AM
-echo COURSE_NAME Tainer - Wazeb
+echo Welcome to $COURSE_NAME training
+echo $COURSE_NAME Training Timings - 10AM
+echo $COURSE_NAME Tainer - Wazeb
