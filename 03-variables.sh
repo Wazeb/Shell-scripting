01a4f4ad58ac92af0ecd2f2ad4e8327403efda67
wazeb <mawazeb123456@gmail.com>
2021-01-03 11:30:56 +0530

    Adding new variables exp.

--- 03-variables.sh
+++ 03-variables.sh
@@ -9,3 +9,16 @@ COURSE_NAME=DevOps
 echo Welcome to $COURSE_NAME training
 echo $COURSE_NAME Training Timings - 10AM
 echo $COURSE_NAME Tainer - Wazeb
+
+# Declare a variable = VARNAME=DATA
+#To access variable = $VARNAME
+
+#What characters you can use in variable names
+# a-z, A-Z, 0-9, -(underscore)
+# Variable name should not start with a number
+    # 1var is wrong
+    # var1 is right
+
+# STYLE in variable NAMES
+# Unix/Linux -> VARNAME (All in capital characters)
+
