823b636db47c673ecab5b69b7fa6be47c4000fa6
Wazeb <mawazeb123456@gmail.com>
2021-01-07 23:01:25 +0530

    Adding new file of exit

---
+++ 09-exit-status.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Every command after its execution it may or may not give output but it gives status in form of a number.
+# This is called as exit status.
+
+#Exit states range from 0-255
+# 0 - global success.
+# 1-255 - partial success / partial failure/ failure
+
+# 0-125 is left for users to use, either can be a command or a script.
+
+# We can use that numbers with the help of exit command.
+
+# exit 0-125 , It is user states
+
+exit
